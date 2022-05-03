e0983909aacd1d7716e23521930b112480fd2a20
gardzielb <bartosz.g.1999@gmail.com>
2022-05-03 19:26:58 +0200

    Initially implemented graph deserialization

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -1,4 +1,6 @@
 ﻿using BTSPEngine;
+using QuickGraph;
+using QuickGraph.Serialization;
 
 namespace Sandbox;
 
@@ -7,6 +9,10 @@ static class Program
 	public static void Main()
 	{
 		var generator = new BTSPGenerator();
-		generator.GenerateToFile(4, "btsp01.xml");
+		generator.GenerateToFile(4, "btsp01");
+
+		var deserializer = new GraphMLDeserializer();
+		var graph = deserializer.Deserialize("btsp01");
+		Console.WriteLine(graph.Vertices);
 	}
 }
