f134ff371075892b620f90234a4eb8172755c772
gardzielb <bartosz.g.1999@gmail.com>
2022-05-08 20:03:21 +0200

    Implemented overall structure of BTSP solver class

---
+++ BTSPEngine/BTSPSolver.cs
@@ -0,0 +1,24 @@
+﻿using QuikGraph;
+
+namespace BTSPEngine;
+
+using WeightedGraph = IVertexListGraph<int, WeightedEdge>;
+using WeightedSparseGraph = UndirectedGraph<int, WeightedEdge>;
+
+public class BTSPSolver
+{
+	public WeightedSparseGraph SolveBTSP(WeightedGraph btsp)
+	{
+		if (btsp.VertexCount < 3)
+			throw new ArgumentException("BTSP graph must contain at least 3 vertices");
+		
+		var (bst, _) = btsp.PrimMST(e => e.Weight);
+		var vertices = btsp.Vertices.ToArray();
+		return FindHamilton(btsp, bst, vertices[0], vertices[1]);
+	}
+
+	private WeightedSparseGraph FindHamilton(WeightedGraph btsp, WeightedSparseGraph bst, int u, int v)
+	{
+		throw new NotImplementedException();
+	}
+}
