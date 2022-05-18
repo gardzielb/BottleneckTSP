3bdd8d6a3dfe9a2a30a41d79a5d81037351289c1
gardzielb <bartosz.g.1999@gmail.com>
2022-05-18 18:31:21 +0200

    Made example generation asynchronous so that main thread is not blocked + added input validation to generator dialog

--- GUI/GeneratorDialog.xaml.cs
+++ GUI/GeneratorDialog.xaml.cs
@@ -1,38 +1,70 @@
 ﻿using System.IO;
 using System.Windows;
 using BTSPEngine;
+using System.ComponentModel;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace GUI;
 
 public partial class GeneratorDialog : Window
 {
+	private readonly CancellationTokenSource cancelTokenSource = new();
+
+	public int VertexCount { get; set; } = 10;
+	public int MaxEdgeWeight { get; set; } = 100;
+	public int InstanceCount { get; set; } = 1;
+
 	public GeneratorDialog()
 	{
 		InitializeComponent();
+		DataContext = this;
 	}
 
-	private void GenerateButton_OnClick(object sender, RoutedEventArgs e)
+	private async void GenerateButton_OnClick(object sender, RoutedEventArgs e)
 	{
-		var vertexCount = int.Parse(VertexCountInput.Text);
-		var maxEdgeWeight = double.Parse(MaxWeightInput.Text);
-		var generator = new BTSPGenerator(maxEdgeWeight);
-		var instanceCount = int.Parse(InstanceCountInput.Text);
+		var generator = new BTSPGenerator(MaxEdgeWeight);
 
 		const string dirName = "generated-examples";
 		Directory.CreateDirectory(dirName);
 
 		int i0 = 0;
-		while (File.Exists($"{MakeFileName(dirName, vertexCount, maxEdgeWeight, i0)}.graphml"))
+		while (File.Exists($"{MakeFileName(dirName, VertexCount, MaxEdgeWeight, i0)}.graphml"))
 			i0++;
 
-		for (int i = 0; i < instanceCount; i++)
-		{
-			generator.GenerateToFile(vertexCount, MakeFileName(dirName, vertexCount, maxEdgeWeight, i0 + i));
-		}
+		generateButton.IsEnabled = false;
+		statusText.Text = $"Trwa generowanie...";
 
+		await Task.Run(() =>
+		{
+			for (int i = 0; i < InstanceCount; i++)
+			{
+				generator.GenerateToFile(VertexCount, MakeFileName(dirName, VertexCount, MaxEdgeWeight, i0 + i));
+			}
+		}, cancelTokenSource.Token);
+
+		statusText.Text = "";
+		generateButton.IsEnabled = true;
 		MessageBox.Show("Pomyślnie wygenerowano przykłady");
 	}
 
 	private string MakeFileName(string dir, int n, double wMax, int i) =>
 		i == 0 ? $"{dir}/btsp-n{n}-w{wMax}" : $"{dir}/btsp-n{n}-w{wMax}-{i}";
+
+	protected override void OnClosing(CancelEventArgs e)
+	{
+		cancelTokenSource.Cancel();
+		base.OnClosing(e);
+	}
+
+	private static bool IsTextAllowed(string text)
+	{
+		return !Regex.IsMatch(text, "[^0-9.-]+", RegexOptions.Compiled);
+	}
+
+	private void TextBox_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+	{
+		e.Handled = !IsTextAllowed(e.Text);
+	}
 }
