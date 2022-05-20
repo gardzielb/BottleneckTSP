2f8066e17f5a437269129bd1973ccdf8bcdb4716
gardzielb <bartosz.g.1999@gmail.com>
2022-05-20 16:03:17 +0200

    Initially implemented graph serialization and deserialization in txt matrix format

---
+++ BTSPEngine/TxtMatrixGraphSerializer.cs
@@ -0,0 +1,51 @@
+﻿using QuikGraph;
+
+namespace BTSPEngine
+{
+	public class TxtMatrixGraphSerializer
+	{
+		public void Serialize(BidirectionalMatrixGraph<WeightedEdge> graph, string path)
+		{
+			using (var writer = new StreamWriter(new FileStream(path, FileMode.Create)))
+			{
+				writer.WriteLine(graph.VertexCount);
+				foreach (int u in graph.Vertices)
+				{
+					foreach (int v in graph.Vertices)
+					{
+						bool isEdge = graph.TryGetEdge(u, v, out WeightedEdge uv);
+						writer.Write(isEdge ? $"{uv.Weight} " : $"0 ");
+					}
+
+					writer.Write('\n');
+				}
+			}
+		}
+
+		public BidirectionalMatrixGraph<WeightedEdge> Deserialize(string path)
+		{
+			using (var reader = new StreamReader(new FileStream(path, FileMode.Open)))
+			{
+				var vertexCount = int.Parse(reader.ReadLine());
+				var graph = new BidirectionalMatrixGraph<WeightedEdge>(vertexCount);
+
+				for (int u = 0; u < vertexCount; u++)
+				{
+					var row = reader.ReadLine().Split(' ');
+					
+					for (int v = 0; v < u; v++)
+					{
+						var weight = double.Parse(row[v]);
+						if (!(weight > 0))
+							continue;
+
+						graph.AddEdge(new WeightedEdge(u, v, weight));
+						graph.AddEdge(new WeightedEdge(v, u, weight));
+					}
+				}
+
+				return graph;
+			}
+		}
+	}
+}
--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -7,9 +7,45 @@ static class Program
 	public static void Main()
 	{
 		var generator = new BTSPGenerator();
-		var btsp = generator.Generate(10);
-		var btspSolver = new BTSPSolver();
-		var hamCycle = btspSolver.SolveBTSP(btsp);
-		Console.WriteLine("dupa");
+		var serializer = new TxtMatrixGraphSerializer();
+
+		var g1 = generator.Generate(10);
+		serializer.Serialize(g1, "g1.txt");
+		var g2 = serializer.Deserialize("g1.txt");
+
+		if (g1.VertexCount != g2.VertexCount)
+		{
+			Console.WriteLine("Wrong vertex count!");
+			return;
+		}
+
+		if (g1.EdgeCount != g2.EdgeCount)
+		{
+			Console.WriteLine("Wrong edge count");
+			return;
+		}
+
+		for (int u = 0; u < g1.VertexCount; u++)
+		{
+			for (int v = 0; v < g1.VertexCount; v++)
+			{
+				if (u == v)
+					continue;
+
+				g1.TryGetEdge(u, v, out WeightedEdge uv1);
+				g2.TryGetEdge(u, v, out WeightedEdge uv2);
+
+				if (uv1.Weight != uv2.Weight)
+				{
+					Console.WriteLine($"({u}, {v}, {uv1.Weight}) != ({u}, {v}, {uv2.Weight})");
+					return;
+				}
+			}
+		}
+
+		// var btsp = generator.Generate(10);
+		// var btspSolver = new BTSPSolver();
+		// var hamCycle = btspSolver.SolveBTSP(btsp);
+		// Console.WriteLine("dupa");
 	}
 }
